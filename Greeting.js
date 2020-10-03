9e574dc9f65c495d13e2a973ac80d6fc719fd63a
MariKohno <info@solowork.net>
2020-10-03 14:54:04 +0900

    add
    条件分岐ができたところでセーブ

---
+++ Greeting.js
@@ -0,0 +1,10 @@
+let age =5;
+if(age >= 10 && age < 20){
+    console.log("10代");
+}else if (age >= 20 && age < 30){
+    console.log("20代");
+} else if (age >= 30 && age < 40 ){
+    console.log("30代");
+}else{
+    console.log("それ以外");
+}
